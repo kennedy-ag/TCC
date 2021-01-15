550630cdfb4188a8309cd65c1a308b4a1b4e9fa4
Kennedy <kennedycontato46@gmail.com>
2021-01-15 01:12:07 -0300

    Adicionados recursos ao grafico

--- tcc/acoes/urls.py
+++ tcc/acoes/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('acoes/<str:codigo_da_acao>', views.acoes, name='acoes'),
+    path('acoes/<str:codigo_da_acao>/<int:dias>', views.acoes, name='acoes'),
 ]
--- tcc/acoes/views.py
+++ tcc/acoes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def index(request): 
     return render(request, 'index.html')
 
-def acoes(request, codigo_da_acao):
+def acoes(request, codigo_da_acao, dias=7):
 
 	from alpha_vantage.timeseries import TimeSeries
 	import json
@@ -21,19 +21,20 @@ def acoes(request, codigo_da_acao):
 	        break
 	      else:
 	        lista_temporal[i] = data[i]
-	        #lista_temporal[i]['0. empresa'] = name_stock(symbol)
+	        lista_temporal[i]['0. empresa'] = name_stock(symbol)
 	        limitador += 1
 	    return lista_temporal
 
-	'''def name_stock(stock_id):
-		nomes = open('nomes.json').read()
+	def name_stock(stock_id):
+		import json
+		nomes = open('static/nomes.json').read()
 		nomes = json.loads(nomes)
 		stock_name = nomes[stock_id]
-		return stock_name'''
+		return stock_name
 
 
 
-	acao = get_stock_info(codigo_da_acao, 15)
+	acao = get_stock_info(codigo_da_acao, dias)
 
 	dados = {}
 	media = 0
@@ -50,7 +51,7 @@ def acoes(request, codigo_da_acao):
 		dados['baixas'].append(round(float(acao[i]['3. low']), 2))
 		dados['fechamentos'].append(round(float(acao[i]['4. close']), 2))
 		dados['volumes'].append(acao[i]['5. volume'])
-		#dados['empresa'] = acao[i]['0. empresa']
+		dados['empresa'] = acao[i]['0. empresa']
 		
 		media += float(acao[i]['4. close'])
 		quantidade += 1
@@ -60,6 +61,7 @@ def acoes(request, codigo_da_acao):
 	dados['codigo'] = codigo_da_acao
 	dados['dias'] = quantidade
 	dados['datas'] = acao.keys()
+	dados['ultimo'] = list(dados['datas'])
 
 
 
